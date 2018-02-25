f89787ca800e0408b58b56290a8d6fde5445a258
jihengwang <jiw367@ucsd.edu>
2018-02-24 16:50:53 -0800

    Fix bug

--- src/main/java/com/around/PostDumpFlow.java
+++ src/main/java/com/around/PostDumpFlow.java
@@ -4,18 +4,6 @@ import com.google.cloud.dataflow.sdk.Pipeline;
 import com.google.cloud.dataflow.sdk.options.PipelineOptions;
 import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
 
-import com.google.api.services.bigquery.model.TableRow;
-import com.google.cloud.bigtable.dataflow.CloudBigtableIO;
-import com.google.cloud.bigtable.dataflow.CloudBigtableScanConfiguration;
-import com.google.cloud.dataflow.sdk.Pipeline;
-import com.google.cloud.dataflow.sdk.io.Read;
-import com.google.cloud.dataflow.sdk.options.PipelineOptions;
-import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
-import com.google.cloud.dataflow.sdk.transforms.DoFn;
-import com.google.cloud.dataflow.sdk.transforms.ParDo;
-import com.google.cloud.dataflow.sdk.values.PCollection;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
 
 import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.api.services.bigquery.model.TableRow;
@@ -39,6 +27,9 @@ import java.util.List;
 
 
 public class PostDumpFlow {
+    private static final String PROJECT_ID = "around-194922";
+    private static final Charset UTF8_CHARSET = Charset.forName("UTF-8");
+
     public static void main(String[] args) {
         // Start by defining the options for the pipeline.
         PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
@@ -46,7 +37,7 @@ public class PostDumpFlow {
         Pipeline p = Pipeline.create(options);
 
         CloudBigtableScanConfiguration config = new CloudBigtableScanConfiguration.Builder()
-                .withProjectI("around-194922")
+                .withProjectId(PROJECT_ID)
                 .withInstanceId("around-post")
                 .withTableId("around-post")
                 .build();
@@ -72,6 +63,21 @@ public class PostDumpFlow {
                 c.output(row);
             }
         }));
+
+        List<TableFieldSchema> fields = new ArrayList<>();
+        fields.add(new TableFieldSchema().setName("postId").setType("STRING"));
+        fields.add(new TableFieldSchema().setName("user").setType("STRING"));
+        fields.add(new TableFieldSchema().setName("message").setType("STRING"));
+        fields.add(new TableFieldSchema().setName("lat").setType("FLOAT"));
+        fields.add(new TableFieldSchema().setName("lon").setType("FLOAT"));
+
+        TableSchema schema = new TableSchema().setFields(fields);
+        bqRows.apply(BigQueryIO.Write
+                .named("Write")
+                .to(PROJECT_ID + ":" + "post_analysis" + "." + "daily_dump_1")
+                .withSchema(schema)
+                .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE)
+                .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED));
 
 
 
